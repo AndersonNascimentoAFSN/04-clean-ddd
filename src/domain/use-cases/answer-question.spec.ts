5feabc9fd844a1e903bb238fdc81f100a3923625
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-04 23:39:16 -0300

    test: add dependency answers-repository

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,14 +1,22 @@
 import { it, describe, expect } from 'vitest'
 
-import { AnswerQuestionUseCase } from './answer-questions'
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswersRepository } from '../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRepository: AnswersRepository = {
+  create: async function(answer: Answer): Promise<void> {
+    return;
+  }
+}
 
 describe('Answer', () => {
-  it('create an answer', () => {
-    const answerQuestion = new AnswerQuestionUseCase()
+  it('create an answer', async () => {
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
 
     const contentText = 'Nova resposta'
 
-    const answer = answerQuestion.execute({
+    const answer = await answerQuestion.execute({
       content: contentText,
       questionId: '1',
       instructorId: '1',
