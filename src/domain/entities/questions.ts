6ec5719bf510b057441ac12f7fd9b513c5bca0cc
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-06 14:29:05 -0300

    refactor: adjust questions for add slug

--- src/domain/entities/questions.ts
+++ src/domain/entities/questions.ts
@@ -1,20 +1,24 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionsProps {
+  id?: string
+  slug: Slug 
   title: string
   content: string
   authorId: string
-  id?: string
 }
 
 export class Question {
+  public id: string
+  public slug: Slug
   public title: string
   public content: string
-  public id: string
   public authorId: string
 
-  constructor({ authorId, content, title, id = randomUUID() }: QuestionsProps) {
+  constructor({ id = randomUUID(), slug, title, content, authorId }: QuestionsProps) {
     this.id = id
+    this.slug = slug
     this.title = title
     this.content = content
     this.authorId = authorId
