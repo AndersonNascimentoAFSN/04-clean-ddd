39418c1c5677cbb7e027b5c3afabe8163fb449cf
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-11 22:24:06 -0300

    feat: fetch recent questions

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.ts
@@ -0,0 +1,24 @@
+import { QuestionsRepository } from '@/domain/forum/application/repositories'
+import { Question } from '@/domain/forum/enterprise/entities'
+
+interface FetchRecentQuestionsUseCaseRequest {
+  page: number
+}
+
+interface FetchRecentQuestionsUseCaseResponse {
+  questions: Question[]
+}
+
+export class FetchRecentQuestionsUseCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+
+  async execute({
+    page,
+  }: FetchRecentQuestionsUseCaseRequest): Promise<FetchRecentQuestionsUseCaseResponse> {
+    const questions = await this.questionRepository.findManyRecent({ page })
+
+    return {
+      questions,
+    }
+  }
+}
