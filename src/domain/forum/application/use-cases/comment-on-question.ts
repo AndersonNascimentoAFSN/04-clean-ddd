0cb852034d130599b690618588802a8e5c38f9c7
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-12 11:19:06 -0300

    feat: comment on question

---
+++ src/domain/forum/application/use-cases/comment-on-question.ts
@@ -0,0 +1,47 @@
+import { UniqueEntityID } from '@/core'
+import {
+  QuestionCommentsRepository,
+  QuestionsRepository,
+} from '@/domain/forum/application/repositories'
+import { QuestionComment } from '@/domain/forum/enterprise/entities'
+
+interface CommentOnQuestionUseCaseRequest {
+  authorId: string
+  questionId: string
+  content: string
+}
+
+interface CommentOnQuestionUseCaseResponse {
+  questionComment: QuestionComment
+}
+
+export class CommentOnQuestionUseCase {
+  constructor(
+    private questionRepository: QuestionsRepository,
+    private questionCommentsRepository: QuestionCommentsRepository,
+  ) {}
+
+  async execute({
+    authorId,
+    questionId,
+    content,
+  }: CommentOnQuestionUseCaseRequest): Promise<CommentOnQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId)
+
+    if (!question) {
+      throw new Error('Question not found.')
+    }
+
+    const questionComment = QuestionComment.create({
+      authorId: new UniqueEntityID(authorId),
+      questionId: new UniqueEntityID(questionId),
+      content,
+    })
+
+    await this.questionCommentsRepository.create(questionComment)
+
+    return {
+      questionComment,
+    }
+  }
+}
