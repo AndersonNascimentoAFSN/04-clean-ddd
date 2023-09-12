fc2727b491995bf8366aa0f76deb93876bd82a90
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-12 14:42:27 -0300

    feat: fetch answer comments

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,29 @@
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories'
+import { AnswerComment } from '@/domain/forum/enterprise/entities'
+
+interface FetchAnswerCommentsUseCaseRequest {
+  answerId: string
+  page: number
+}
+
+interface FetchAnswerCommentsUseCaseResponse {
+  answerComments: AnswerComment[]
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
+
+  async execute({
+    page,
+    answerId,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments =
+      await this.answerCommentsRepository.findManyByAnswerId(answerId, {
+        page,
+      })
+
+    return {
+      answerComments,
+    }
+  }
+}
