8ae4d7b63a19f5ed4d69be0a133c1e5da670aaff
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-13 09:54:55 -0300

    test: create question with attachments

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { CreateQuestionUseCase } from './create-question'
 import { InMemoryQuestionsRepository } from 'test'
+import { UniqueEntityID } from '@/core'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: CreateQuestionUseCase
@@ -18,9 +19,31 @@ describe('Create Question', () => {
       title: 'Nova pergunta',
       content: contentText,
       authorId: '1',
+      attachmentsIds: [],
     })
 
     expect(result.isRight()).toBe(true)
     expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(0)
+  })
+
+  it('should be able to create a question with attachments', async () => {
+    const contentText = faker.lorem.text()
+
+    const result = await sut.execute({
+      title: 'Nova pergunta',
+      content: contentText,
+      authorId: '1',
+      attachmentsIds: ['1', '2'],
+    })
+
+    expect(result.isRight()).toBe(true)
+    expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question)
+
+    expect(inMemoryQuestionsRepository.items[0].attachments).toHaveLength(2)
+    expect(inMemoryQuestionsRepository.items[0].attachments).toEqual([
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
   })
 })
