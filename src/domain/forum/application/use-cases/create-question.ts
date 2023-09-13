e9778a2060b26660ff36ed28f86f7ba26a13ab2e
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-13 09:54:45 -0300

    feat: create question with attachments

--- src/domain/forum/application/use-cases/create-question.ts
+++ src/domain/forum/application/use-cases/create-question.ts
@@ -1,11 +1,13 @@
 import { Either, UniqueEntityID, right } from '@/core'
 import { QuestionsRepository } from '@/domain/forum/application/repositories'
 import { Question } from '@/domain/forum/enterprise/entities'
+import { QuestionAttachment } from '../../enterprise/entities/question-attachment'
 
 interface CreateQuestionUseCaseRequest {
+  authorId: string
+  attachmentsIds: string[]
   title: string
   content: string
-  authorId: string
 }
 
 type CreateQuestionUseCaseResponse = Either<
@@ -19,15 +21,25 @@ export class CreateQuestionUseCase {
   constructor(private questionRepository: QuestionsRepository) {}
 
   async execute({
+    authorId,
+    attachmentsIds,
     title,
     content,
-    authorId,
   }: CreateQuestionUseCaseRequest): Promise<CreateQuestionUseCaseResponse> {
     const question = Question.create({
       title,
       content,
       authorId: new UniqueEntityID(authorId),
     })
+
+    const questionsAttachments = attachmentsIds.map((attachmentId) => {
+      return QuestionAttachment.create({
+        attachmentId: new UniqueEntityID(attachmentId),
+        questionId: question.id,
+      })
+    })
+
+    question.attachments = questionsAttachments
 
     await this.questionRepository.create(question)
 
