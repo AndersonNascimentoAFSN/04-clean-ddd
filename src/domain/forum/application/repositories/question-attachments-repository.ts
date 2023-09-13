b0f4d5e217f7da92349b35beb72f8f54ff7614e0
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-13 13:39:52 -0300

    feat: exclude attachments question

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -2,4 +2,5 @@ import { QuestionAttachment } from '@/domain/forum/enterprise/entities'
 
 export interface QuestionAttachmentsRepository {
   findManyByQuestionId(questionId: string): Promise<Array<QuestionAttachment>>
+  deleteManyByQuestionId(questionId: string): Promise<void>
 }
