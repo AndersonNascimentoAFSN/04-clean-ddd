bcffb487a2c33b710fa0e88b4c9a141e908f4065
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-14 14:56:22 -0300

    test: flow send notification

--- test/repositories/forum/in-memory-answers-repository.ts
+++ test/repositories/forum/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import {
   AnswerAttachmentsRepository,
@@ -28,6 +29,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer) {
     this.items.push(answer)
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 
   async delete(answer: Answer) {
@@ -42,5 +45,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
     this.items[itemIndex] = answer
+
+    DomainEvents.dispatchEventsForAggregate(answer.id)
   }
 }
